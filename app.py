61137d666436d85210c10d27586c818014447460
dmichael8122 <dmichael8122@gmail.com>
2025-08-07 19:31:47 -0700

    gitignore and SSO port

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="static")
+from flask import Blueprint
+from dotenv import load_dotenv
+import os
 
-@app.route('/')
+load_dotenv()
+APP_ROOT = os.environ.get("APP_ROOT", "")  # Set to '/dummy-app' in prod, '' in dev
+app = Flask(__name__, static_url_path=f"{APP_ROOT}/static")
+main_bp = Blueprint('main', __name__, url_prefix=APP_ROOT)
+
+@main_bp.route('/')
 def home():
     return render_template('index.html')
 
